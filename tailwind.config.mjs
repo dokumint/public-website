1d8c6ae0528e5165571bc11d8f5de765768bd4bb
burtonmsp <100707360+burtonmsp@users.noreply.github.com>
2025-12-16 19:01:03 +0000

    Add tailwind base

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "media",
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
